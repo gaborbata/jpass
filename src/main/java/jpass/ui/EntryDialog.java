98f507e8bef4c80f6b017348c48e93c8d76c86a2
Gabor Bata <bata.gabor@gmail.com>
2015-03-02 11:16:53 +0100

    Changed default font to sans serif on notes field of entry dialog.

--- src/main/java/jpass/ui/EntryDialog.java
+++ src/main/java/jpass/ui/EntryDialog.java
@@ -32,6 +32,7 @@ package jpass.ui;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -152,6 +153,7 @@ public class EntryDialog extends JDialog implements ActionListener {
         this.notesPanel.setBorder(new EmptyBorder(0, 5, 0, 5));
         this.notesPanel.add(new JLabel("Notes:"), BorderLayout.NORTH);
         this.notesField = TextComponentFactory.newTextArea();
+        this.notesField.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
         this.notesField.setLineWrap(true);
         this.notesField.setWrapStyleWord(true);
         this.notesPanel.add(new JScrollPane(this.notesField), BorderLayout.CENTER);

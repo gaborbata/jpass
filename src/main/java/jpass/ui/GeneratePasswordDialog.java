d55bfd04dd02cf180f9e388df4e9ff2adc448a56
Gabor Bata <bata.gabor@gmail.com>
2024-03-05 13:37:59 +0100

    update copyright header

--- src/main/java/jpass/ui/GeneratePasswordDialog.java
+++ src/main/java/jpass/ui/GeneratePasswordDialog.java
@@ -1,7 +1,7 @@
 /*
  * JPass
  *
- * Copyright (c) 2009-2023 Gabor Bata
+ * Copyright (c) 2009-2024 Gabor Bata
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without

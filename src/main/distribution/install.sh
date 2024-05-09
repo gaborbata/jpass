d001610142916e51375915c163fc6f5715fa4524
Gabor Bata <gaborbata@users.noreply.github.com>
2024-05-09 21:30:07 +0200

    Update install.sh

--- src/main/distribution/install.sh
+++ src/main/distribution/install.sh
@@ -2,21 +2,6 @@
 
 # JPass installation script for Linux
 
-# check if java executable exists
-if [ -d "$JAVA_HOME" -a -x "$JAVA_HOME/bin/java" ]; then
-    JAVACMD="$JAVA_HOME/bin/java"
-else
-    JAVACMD=java
-fi
-
-$JAVACMD -version >/dev/null 2>&1
-if [ "$?" != "0" ]; then
-  echo "Install Java (JDK or JRE) if you do not already have. JPass will not work without it."
-  echo "Please make sure PATH, or JAVA_HOME environment variables point to a valid Java installation."
-  echo "Could not execute JPass (exit: $?)"
-  exit
-fi
-
 # detect the absolute path of jpass
 JPASS_HOME=`dirname "$0"`
 

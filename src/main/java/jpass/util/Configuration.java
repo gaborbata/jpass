a2d9f76f0972bbe46b269edbc0b1a92fb33baa20
Edi Morales <edi.moralcruz@gmail.com>
2025-02-20 03:41:59 -0600

    Addressed review comments

--- src/main/java/jpass/util/Configuration.java
+++ src/main/java/jpass/util/Configuration.java
@@ -32,12 +32,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Optional;
 import java.io.OutputStream;
 import java.nio.file.Files;
-import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Optional;
+import java.util.Properties;
 
 /**
  * Class for loading configurations from {@code jpass.properties} or system properties.
